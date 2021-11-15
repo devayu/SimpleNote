6a9eb4006107c48323a55e00b507ba9e9f64a251
Siddharth <sgautam@mphrx.com>
2021-11-15 14:17:28 +0530

    Added SignUp page and added it in NavigationController

--- SimpleNote/LoginViewController.swift
+++ SimpleNote/LoginViewController.swift
@@ -12,8 +12,19 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+    
+    @IBAction func didTapSignUp(){
+        let su = storyboard?.instantiateViewController(withIdentifier: "signup_vc") as! SignUpViewController
+        
+        print("Tapped button")
+        navigationController?.pushViewController(su, animated: true)
+        
     }
 
-
 }
+
+
+
 
